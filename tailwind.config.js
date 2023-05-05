29b4e4c58b54e95c335b72578f6b549b861ae23d
Shin-Taro <sh.takahashi.113@gmail.com>
2023-05-05 17:06:16 +0900

    chore: update tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,28 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    screens: {
+      sp: { max: "767px" },
+      pc: { min: "768px" },
     },
+    // カラーパレット定義( https://www.figma.com/file/8JNl8iF4w9cDaanBaXauAD/Blog?node-id=18-182&t=hP9Kfvck2BC3y6gR-0 )
+    colors: {
+      main: "#117099",
+      accent: "#A94949",
+      backGround: "#272727",
+      font: "FFFFFF",
+      sub: "3C3C3C",
+    },
+    // フォントサイズ定義( https://www.figma.com/file/8JNl8iF4w9cDaanBaXauAD/Blog?node-id=21-48&t=hP9Kfvck2BC3y6gR-0 )
+    fontSize: {
+      first: "32px",
+      second: "24px",
+      third: "20px",
+      fourth: "16px",
+      fifth: "14px",
+      sixth: "12px",
+    },
+    extend: {},
   },
   plugins: [],
 }
