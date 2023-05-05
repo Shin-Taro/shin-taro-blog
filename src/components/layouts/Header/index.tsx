a3a606495c05733fbc59b953ac91d42d85b4bd2a
Shin-Taro <sh.takahashi.113@gmail.com>
2023-05-05 23:18:26 +0900

    feat: adjust header sp style

--- src/components/layouts/Header/index.tsx
+++ src/components/layouts/Header/index.tsx
@@ -3,16 +3,16 @@ import { TwitterIcon } from "@/icons/Twitter"
 import { FC } from "react"
 
 export const Header: FC = () => (
-  <header className="rounded-[10px] bg-main px-[30px] py-[13px] shadow-dropMain">
+  <header className="rounded-[10px] bg-main shadow-dropMain sp:px-[20px] sp:py-[15px] pc:px-[30px] pc:py-[13px]">
     <div className="flex justify-between">
-      <h1 className="text-first">shin-taro / blog</h1>
+      <h1 className="text-first sp:text-second">shin-taro / blog</h1>
       <ul className="flex justify-between">
         <li className="flex items-center">
           <a href="https://github.com/Shin-Taro" target="_blank" rel="noopener noreferrer">
             <GithubIcon />
           </a>
         </li>
-        <li className="mx-[20px] flex items-center">
+        <li className="mx-[20px] flex items-center sp:mx-[15px]">
           {/* TODO: twitterアカウント作る */}
           <a href="https://github.com/Shin-Taro" target="_blank" rel="noopener noreferrer">
             <TwitterIcon />
