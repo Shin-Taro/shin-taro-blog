61aa3bc70c1cc8bdab0a2f31ddb132d765feab17
Shin-Taro <sh.takahashi.113@gmail.com>
2023-05-06 02:38:03 +0900

    feat: adjust Profile component styles

--- src/components/elements/Profile/index.tsx
+++ src/components/elements/Profile/index.tsx
@@ -5,7 +5,7 @@ export const Profile: FC = () => (
     <div className="mx-auto h-[100px] w-[100px]">
       <img className="h-full w-full rounded-full" src="/images/profile.webp" alt="shin-taroのプロフィール画像" />
     </div>
-    <h4 className="mt-[10px] text-fifth">shin-taro</h4>
-    <p className="mt-[5px] text-fifth">フロントエンドをやっています。 このブログは気ままに書いているので更新頻度は疎らです。</p>
+    <h4 className="mt-[10px] text-fourth font-medium">shin-taro</h4>
+    <p className="mt-[8px] text-fifth">フロントエンドをやっています。 このブログは気ままに書いているので更新頻度は疎らです。</p>
   </div>
 )
