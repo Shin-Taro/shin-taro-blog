dfe4cf14d7b13fc96aac1dc833ba58f7a201b3e2
Shin-Taro <sh.takahashi.113@gmail.com>
2023-05-05 22:49:44 +0900

    chore: adjust tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,12 +16,15 @@ module.exports = {
     },
     // フォントサイズ定義( https://www.figma.com/file/8JNl8iF4w9cDaanBaXauAD/Blog?node-id=21-48&t=hP9Kfvck2BC3y6gR-0 )
     fontSize: {
-      first: "32px",
-      second: "24px",
-      third: "20px",
-      fourth: "16px",
-      fifth: "14px",
-      sixth: "12px",
+      first: "3.2rem",
+      second: "2.4rem",
+      third: "2rem",
+      fourth: "1.6rem",
+      fifth: "1.4rem",
+      sixth: "1.2rem",
+    },
+    boxShadow: {
+      dropMain: "1px 3px 3px rgba(255, 255, 255, 0.15)",
     },
     extend: {},
   },
