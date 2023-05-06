fffdc19512db3673b828b74041c89304f16e0c40
Shin-Taro <sh.takahashi.113@gmail.com>
2023-05-06 15:00:19 +0900

    feat: implement Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,31 @@
 import { NextPage } from "next"
+import { Home } from "@/components/templates/Home"
+import { formatDate } from "@/modules/formatDate"
 
-const Home: NextPage = () => (
-  <main>
-    <section>
-      <h1 className="text-first">Home</h1>
-      <div>Welcome to my blog</div>
-    </section>
-  </main>
-)
+// FIXME: 後で消す
+const dummy = {
+  articles: [
+    {
+      id: "dummy1",
+      createDate: formatDate("2022-01-01T00:00:00.000Z"),
+      title: "Dummy article title",
+      body: "<p>Plain text is available using the fmt operator.</p>",
+      tags: ["React", "Next.js"],
+      excerpt:
+        "Dummy article excerpt.Dummy article excerpt.Dummy article excerpt.Dummy article excerpt.Dummy article excerpt.Dummy article excerpt.Dummy article excerpt.Dummy article excerpt.Dummy article excerpt.",
+    },
+    {
+      id: "dummy2",
+      createDate: formatDate("2022-01-01T00:00:00.000Z"),
+      title: "Dummy article title",
+      body: "<p>Plain text is available using the fmt operator.</p>",
+      tags: ["React", "Next.js"],
+      excerpt:
+        "Dummy article excerpt.Dummy article excerpt.Dummy article excerpt.Dummy article excerpt.Dummy article excerpt.Dummy article excerpt.Dummy article excerpt.Dummy article excerpt.Dummy article excerpt.",
+    },
+  ],
+}
 
-export default Home
+const HomePage: NextPage = () => <Home articleListProps={dummy} />
+
+export default HomePage
