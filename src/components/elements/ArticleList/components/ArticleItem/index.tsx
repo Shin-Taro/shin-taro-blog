9ecb5fcea8c3e143ff9ac8fa733e5c87cb146a43
Shin-Taro <sh.takahashi.113@gmail.com>
2023-05-06 17:46:06 +0900

    feat: add link to ArticleItem

--- src/components/elements/ArticleList/components/ArticleItem/index.tsx
+++ src/components/elements/ArticleList/components/ArticleItem/index.tsx
@@ -1,14 +1,17 @@
 import { Tag } from "@/components/elements/Tag"
 import { TagIcon } from "@/icons/Tag"
 import { Article } from "@/models/Article"
+import Link from "next/link"
 import { FC } from "react"
 
 // TODO: リンク追加
 export const ArticleItem: FC<Article> = (props) => {
-  const { title, excerpt, createDate, tags } = props
+  const { title, excerpt, createDate, tags, id } = props
   return (
     <article>
-      <h2 className="text-second sp:text-third">{title}</h2>
+      <h2 className="text-second sp:text-third">
+        <Link href={`/articles/${id}`}>{title}</Link>
+      </h2>
       <div className="mt-[15px] flex">
         <p className="text-fifth">{createDate}</p>
         <div className="ml-[15px]">
@@ -17,7 +20,9 @@ export const ArticleItem: FC<Article> = (props) => {
         <ul className="ml-[10px] flex">
           {tags.map((tag, index) => (
             <li key={tag} className={index ? "ml-[10px]" : ""}>
-              <Tag>{tag}</Tag>
+              <Link href={`/search/${tag}`}>
+                <Tag>{tag}</Tag>
+              </Link>
             </li>
           ))}
         </ul>
