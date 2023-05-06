dee3e416e1509248a9b4e83d84eb09923d5d3af0
Shin-Taro <sh.takahashi.113@gmail.com>
2023-05-06 15:00:03 +0900

    feat: implement Home templates

---
+++ src/components/templates/Home/index.tsx
@@ -0,0 +1,15 @@
+import { ComponentPropsWithoutRef, FC } from "react"
+import { ArticleList } from "@/components/elements/ArticleList"
+
+type Props = {
+  articleListProps: ComponentPropsWithoutRef<typeof ArticleList>
+}
+
+export const Home: FC<Props> = (props) => (
+  <section>
+    <h1 className="text-center text-first sp:text-second">記事一覧</h1>
+    <div className="mt-[30px]">
+      <ArticleList {...props.articleListProps} />
+    </div>
+  </section>
+)
