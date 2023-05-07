726375c7e05c4be82f3ceac802cd132cf9960a4f
Shin-Taro <sh.takahashi.113@gmail.com>
2023-05-07 16:14:06 +0900

    feat: implement data fetching on Article page

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,18 +1,24 @@
-import { NextPage } from "next"
-import { formatDate } from "@/modules/formatDate"
 import { ArticleTemplate } from "@/components/templates/Articles/Id"
+import { newtApiClient } from "@/libs/newt/newtApiClient"
+import { ArticleResponse } from "@/libs/newt/types"
+import { AppUid } from "@/const/AppUid"
+import { ModelUid } from "@/const/ModelUid"
+import { convertArticle } from "@/modules/convertToArticleList"
 
-// FIXME: 後で消す
-const dummy = {
-  id: "dummy1",
-  createDate: formatDate("2022-01-01T00:00:00.000Z"),
-  title: "Dummy article title",
-  body: '<h2>Sammary</h2>\n<p>this is test post .<br>\nthis is inline code test . <code>const hoge = &quot;test&quot;</code></p>\n<pre><code class="language-ts">const fuga: string = &quot;fuga&quot;\n</code></pre>\n<pre><code class="language-diff">+ const Piyo: number = 0\n- const puyo = 0\n</code></pre>\n<p><img src="https://storage.googleapis.com/p_6454c5577701b2db02707526/59657d94-aa48-4b72-824b-9a8bbc764fb0/profile.webp" alt="profile.webp"></p>\n',
-  tags: ["React", "Next"],
-  excerpt:
-    "Dummy article excerpt.Dummy article excerpt.Dummy article excerpt.Dummy article excerpt.Dummy article excerpt.Dummy article excerpt.Dummy article excerpt.Dummy article excerpt.Dummy article excerpt.",
+type Props = {
+  params: {
+    id: string
+  }
 }
 
-const ArticlePage: NextPage = () => <ArticleTemplate {...dummy} />
+const ArticlePage = async (props: Props) => {
+  const {
+    params: { id },
+  } = props
+  const response = await newtApiClient.getContent<ArticleResponse>({ appUid: AppUid, modelUid: ModelUid.ARTICLE, contentId: id })
+  const article = convertArticle(response)
+
+  return <ArticleTemplate {...article} />
+}
 
 export default ArticlePage
