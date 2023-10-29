8e218ead45d71ae57cb47c72918d4de4c8708ba4
Shin-Taro <sh.takahashi.113@gmail.com>
2023-10-29 20:19:50 +0900

    feat: fix date format

--- src/modules/formatDate/index.ts
+++ src/modules/formatDate/index.ts
@@ -1,3 +1,3 @@
 import { format } from "date-fns"
 
-export const formatDate = (dateString: string): string => format(new Date(dateString), "yyyy/mm/dd")
+export const formatDate = (dateString: string): string => format(new Date(dateString), "yyyy/MM/dd")
