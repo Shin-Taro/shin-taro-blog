f4f344ef9685288f7f3ff7ef493d70d16d095eb3
Shin-Taro <sh.takahashi.113@gmail.com>
2024-03-17 21:48:15 +0900

    style: fix Tag component className format

--- src/components/elements/Tag/index.tsx
+++ src/components/elements/Tag/index.tsx
@@ -1,7 +1,21 @@
 import { FC, PropsWithChildren } from "react"
 
 export const Tag: FC<PropsWithChildren> = ({ children }) => (
-  <span className="rounded-[3px] bg-font px-[5px] py-[2px] align-middle text-sixth font-medium text-backGround sp:px-[4px] sp:py-[1px] sp:font-normal">
+  <span
+    className={`
+      rounded-[3px]
+      bg-font
+      px-[5px]
+      py-[2px]
+      align-middle
+      text-sixth
+      font-medium
+      text-backGround
+      sp:px-[4px]
+      sp:py-[1px]
+      sp:font-normal
+    `}
+  >
     {children}
   </span>
 )
